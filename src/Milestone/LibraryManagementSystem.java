ae9a2e71b6a6c0fc6e1d9a92d0a532790596100e
Nikhil Nandigoud <nikhilnandigoud@gmail.com>
2025-11-17 09:17:44 +0530

    commit1

---
+++ src/Milestone/LibraryManagementSystem.java
@@ -0,0 +1,19 @@
+package Milestone;
+
+import java.util.*;
+
+public class LibraryManagementSystem {
+
+
+    List<Book> bookInventory;
+    List<User> registeredUsers;
+  void   addBook(Book book){
+
+
+
+    }
+
+    void registerUser(User user){
+
+    }
+}
